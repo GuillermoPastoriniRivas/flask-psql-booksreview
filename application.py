9e265f184e462183d58ddf9ec20fe3e27c2c9ea7
GuillermoPastoriniRivas <guillepastorini@hotmail.com>
2020-03-23 18:44:08 -0300

    Primer commit

---
+++ application.py
@@ -0,0 +1,46 @@
+import os
+
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    books = db.execute("SELECT isbn, title FROM books LIMIT 30").fetchall()
+    return render_template("index.html", books=books)
+
+@app.route("/book", methods=["POST"])
+def usuario():
+    """Book a flight."""
+
+    # Get form information.
+    name = request.form.get("name")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    # Make sure flight exists.
+    if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+        return render_template("error.html", message="No such flight with that id.")
+    db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+            {"name": name, "flight_id": flight_id})
+    db.commit()
+    return render_template("success.html")
